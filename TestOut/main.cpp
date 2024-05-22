204c311b60295d7d744c053f579fdeb7cea0a32f
godyoung12 <godyoung12@gmail.com>
2024-05-22 22:03:39 +0900

    텍스트 띄우기,
    WndProc에 (DefwindowProc(hWnd, iMessage, wParam, lParam)) 안써서 창이 안나오는 현상이 발생했었음 <- 다음에 주의할 것.

---
+++ TestOut/main.cpp
@@ -0,0 +1,55 @@
+#include <Windows.h>
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = TEXT("TextOut");
+
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow) {
+	HWND hWnd;
+	MSG Message;
+	WNDCLASS WndClass;
+	g_hInst = hInstance;
+
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION
+	);
+	WndClass.hInstance = hInstance;
+	
+	WndClass.lpfnWndProc = WndProc;
+
+	WndClass.lpszClassName = lpszClass;
+	WndClass.lpszMenuName = NULL;
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	RegisterClass(&WndClass);
+
+	hWnd = CreateWindow(lpszClass, lpszClass, WS_OVERLAPPEDWINDOW | WS_VSCROLL, 0, 0, 640, 480, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+
+	while (GetMessage(&Message, NULL, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+
+	return (int)Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam) {
+	HDC hdc;
+
+	switch (iMessage) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_LBUTTONDOWN:
+		hdc = GetDC(hWnd);
+		TextOut(hdc, 100, 100, TEXT("Beautiful Korea"), 15);
+		ReleaseDC(hWnd, hdc);
+		return 0;
+	}
+
+	return (DefWindowProc(hWnd, iMessage, wParam, lParam));
+}
